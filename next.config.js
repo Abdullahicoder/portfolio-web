11618ba9bac4fe2a0e423f4f08cced85e4b2ef91
Abdullaahi Ali <abdullaahiali@Abdullaahis-MacBook-Air.local>
2025-06-12 12:55:50 +0300

    🔧 configure GitHub Pages deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'portfolio-web';
+
 const nextConfig = {
+  // Enable static HTML export
+  output: 'export',
+  // Ensure trailing slashes for GitHub Pages routing
+  trailingSlash: true,
+  // If in production, set basePath and assetPrefix to the repo name
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+
   images: {
     remotePatterns: [
       {
